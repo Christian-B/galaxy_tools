d16a217d1771c1152704e949943a0622868310e6
= <christian.brenninkmeijer@manchester.ac.uk>
2015-04-29 17:23:21 +0100

    Style fixes for flake8

--- fasta/fastq_summary.py
+++ fasta/fastq_summary.py
@@ -1,5 +1,7 @@
 from collections import Counter
-from optparse import OptionParser, OptionGroup
+from optparse import OptionParser
+import sys
+
 
 class Fastq:
 
@@ -8,32 +10,33 @@ class Fastq:
         self.sequence = ""
         self.quality = ""
         self.plusLine = False
-        
+
     def addLine(self, line):
         if self.plusLine:
             self.addQualityLine(line)
         else:
             self.addSequenceLine(line)
-                
+
     def addSequenceLine(self, line):
-        self.sequence+= line
-          
+        self.sequence += line
+
     def addQualityLine(self, line):
         self.quality += line
         if len(self.sequence) > len(self.quality):
-            print >> sys.stderr, "Unexcpected format. Quality appears longer than sequence."
+            error = "Unexpected format. Quality appears longer than sequence."
+            print >> sys.stderr, error
             print >> sys.stderr, "Read previous fastq as:"
-            print >> sys.stderr, fastq
-            sys.exit(1) 
-        
+            print >> sys.stderr, self.sequence
+            sys.exit(1)
+
     def plusLineExists(self, line):
         if self.plusLine:
             return True
         self.plusLine = line
-        return False   
-          
+        return False
+
     def ___repr__(self):
-        reply  = "@" + self.name + "/n"
+        reply = "@" + self.name + "/n"
         reply += self.sequence + "/n"
         reply += "+/n"
         reply += self.quality
@@ -41,15 +44,16 @@ class Fastq:
 
 _counterKeys = None
 
+
 class FastqSummary:
 
     def __init__(self, fastq):
         if fastq:
-            self.name = fastq.name 
+            self.name = fastq.name
             self.length = len(fastq.sequence)
             self.sequence_counter = Counter(fastq.sequence)
-            self.minQuality = min(fastq.quality)        
-            self.maxQuality = max(fastq.quality)        
+            self.minQuality = min(fastq.quality)
+            self.maxQuality = max(fastq.quality)
             if _allSummary.name:
                 _allSummary.length += self.length
                 _allSummary.sequence_counter.update(self.sequence_counter)
@@ -57,55 +61,53 @@ class FastqSummary:
                     _allSummary.minQuality = self.minQuality
                 if _allSummary.maxQuality < self.maxQuality:
                     _allSummary.maxQuality = self.maxQuality
-                _counterKeys = None
             else:
                 _allSummary.name = "All"
                 _allSummary.length = self.length
                 _allSummary.sequence_counter = Counter(fastq.sequence)
                 _allSummary.minQuality = self.minQuality
                 _allSummary.maxQuality = self.maxQuality
-                _counterKeys = None
         else:
-             self.name = "" 
-             self.length = 0 
-             self.sequence_counter = Counter()
-             self.minQuality = 0
-             self.maxQuality = 0
-             
+            self.name = ""
+            self.length = 0
+            self.sequence_counter = Counter()
+            self.minQuality = 0
+            self.maxQuality = 0
+
     def ___repr__(self):
         reply = "FastqSummary(" + self.name + "," + str(self.length) + ","
-        reply += repr(self.counter) 
+        reply += repr(self.counter)
         reply += str(self.minQuality) + "," + str(self.maxQuality) + ")"
         return reply
-        
+
     def __str__(self):
-        reply = str(self.length) + "\t" 
+        reply = str(self.length) + "\t"
         reply += str(self.minQuality) + "\t" + str(self.maxQuality)
-        for key in counterKeys():            
-            reply+= "\t" + str(self.sequence_counter[key]) 
-        reply += "\t" + self.name 
-        return reply 
-      
+        for key in counterKeys():
+            reply += "\t" + str(self.sequence_counter[key])
+        reply += "\t" + self.name
+        return reply
+
 _allSummary = FastqSummary(None)
+
 
 def counterKeys():
     global _counterKeys
     if not _counterKeys:
         _counterKeys = sorted(_allSummary.sequence_counter.keys())
     return _counterKeys
-            
+
+
 def strHeader():
-    line = ["Length","Min Q","max Q"]
+    line = ["Length", "Min Q", "max Q"]
     line.extend(counterKeys())
     line.append("Name")
     return "\t".join(line)
-          
-def summary(input_path, output_path):
-        
-    print "Summarizing ", input_path, "to", output_path
-   
-    summaries = []
 
+
+def summary(input_path, output_path):
+    print "Summarizing ", input_path, "to", output_path
+    summaries = []
     #read data
     with open(input_path, 'r') as input_file:
         fastq = None
@@ -119,42 +121,43 @@ def summary(input_path, output_path):
                         fastq = Fastq(stripped[1:])
                     else:
                         fastq.addQualityLine(stripped)
-                else: #first fastq       
-                    fastq = Fastq(stripped[1:])     
+                else:  # first fastq
+                    fastq = Fastq(stripped[1:])
             elif stripped.startswith("+"):
                 if fastq.plusLineExists(stripped):
                     fastq.addQualityLine(stripped)
             else:
                 #Fastq will know if it is sequence or quality
                 fastq.addLine(stripped)
-        #save last Fastq       
+        #save last Fastq
         summaries.append(FastqSummary(fastq))
-                        
+
     with open(output_path, 'w') as output_file:
-       output_file.write(strHeader())
-       output_file.write("\n")
-       output_file.write(str(_allSummary))
-       output_file.write("\n")
-       for summary in summaries:
-           output_file.write(str(summary))
-           output_file.write("\n")
-        
+        output_file.write(strHeader())
+        output_file.write("\n")
+        output_file.write(str(_allSummary))
+        output_file.write("\n")
+        for summary in summaries:
+            output_file.write(str(summary))
+            output_file.write("\n")
+
 if __name__ == '__main__':
     usage = "usage: %prog [options] INPUT_FILE"
     # define options
     parser = OptionParser(usage=usage)
-    parser.add_option("-o", "--output", dest="output_path", 
-                      help="Path to file, where the output should be written. Defaults to counts.tsv", 
+    help = "Path to file, where the output should be written. "
+    help += "Defaults to counts.tsv"
+    parser.add_option("-o", "--output", dest="output_path",
+                      help=help,
                       default="lengths.tsv")
-                      
+
     # parse
     options, args = parser.parse_args()
 
     #Check exactly one input_file specified
     if len(args) < 1:
-        parser.error("ERROR! No INPUT_FILE specified.")   
+        parser.error("ERROR! No INPUT_FILE specified.")
     elif len(args) > 1:
-        parser.error("ERROR! Multiple values for INPUT_FILE found.")   
-        
-    summary(args[0], options.output_path)
+        parser.error("ERROR! Multiple values for INPUT_FILE found.")
 
+    summary(args[0], options.output_path)
